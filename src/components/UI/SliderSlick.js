2b7600eb81494b142da1e71ad7664dcf7871ae2f
darshan <darshan@gmail.com>
2023-08-26 19:25:14 +0530

    add slider

---
+++ src/components/UI/SliderSlick.js
@@ -0,0 +1,98 @@
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
+import React from 'react'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+const SliderSlick = ({ latest }) => {
+    const router = useRouter();
+    const settings = {
+        dots: false,
+        centerpedding:10,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+
+
+
+    };
+
+    return (
+        <>
+            <div className='col-span-12 sm:col-span-12 gap-7'>
+            <Slider {...settings}>
+
+                {latest.map((item, ind) => (
+                    <div
+                        key={ind}
+                        className=" col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 bg-white border border-gray-200 rounded-lg shadow text-dark mt-5 sm:mt-0"
+                    >
+                        <a href="#">
+                            <img
+                                className="rounded-t-lg w-full h-[200px]"
+                                src={item.coverImage}
+                                alt=""
+                            />
+                        </a>
+                        <div className="p-5">
+                            <a href="#">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-black">
+                                    {item.title}
+                                </h5>
+                            </a>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                {item.description}
+                            </p>
+                            <div
+                                onClick={() => router.push(`/category/latests/${item.slug}`)}
+                                href="#"
+                                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                Read more
+                                <svg
+                                    className="w-3.5 h-3.5 ml-2"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 14 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+            </Slider>
+            </div>
+        </>
+    )
+}
+
+export default SliderSlick
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,8 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import Loader from "./loading";
 import Pagination from "@/components/UI/Pagination";
-import { getDocuments } from "outstatic/server";
-import Link from "next/link";
+import { getDocuments } from "outstatic/server";;
+import SliderSlick from "@/components/UI/SliderSlick";
 
 function MyApp({ Component, pageProps, router, posts }) {
   const [loading, setLoading] = React.useState(false);
@@ -21,8 +21,8 @@ function MyApp({ Component, pageProps, router, posts }) {
       setTotalPages(Math.ceil(localStorage.getItem("len") / 10));
       setIsSingal(
         router.pathname.includes("[slug]") ||
-          router.pathname.includes("info") ||
-          router.query.s
+        router.pathname.includes("info") ||
+        router.query.s
       );
     }
   }, [isCilent, loading, router.pathname]);
@@ -84,55 +84,17 @@ function MyApp({ Component, pageProps, router, posts }) {
                   <Sidebar />
                 </div>
               </div>
-
               {isSingal && (
-                <div className="sm:grid grid-cols-12 gap-7 rounded-xl w-full items-center my-12">
-                  {latest.map((item, ind) => (
-                    <div
-                      key={ind}
-                      className="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 bg-white border border-gray-200 rounded-lg shadow text-dark mt-5 sm:mt-0"
-                    >
-                      <a href="#">
-                        <img
-                          className="rounded-t-lg w-full h-[200px]"
-                          src={item.coverImage}
-                          alt=""
-                        />
-                      </a>
-                      <div className="p-5">
-                        <a href="#">
-                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-black">
-                            {item.title}
-                          </h5>
-                        </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                          {item.description}
-                        </p>
-                        <Link
-                          href={`/category/latests/${item.slug}`}
-                          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        >
-                          Read more
-                          <svg
-                            className="w-3.5 h-3.5 ml-2"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 14 10"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M1 5h12m0 0L9 1m4 4L9 9"
-                            />
-                          </svg>
-                        </Link>
-                      </div>
+                <>
+                  <div className="sm:grid grid-cols-12 gap-7 rounded-xl w-full items-center mt-12">
+                    <div className="col-span-12  mb-5 sm:mb-0 text-3xl font-bold">
+                      More Suggestions
                     </div>
-                  ))}
-                </div>
+                  </div>
+                  <div className="sm:grid grid-cols-12 gap-7 rounded-xl w-full items-center my-12">
+                    <SliderSlick latest={latest} />
+                  </div>
+                </>
               )}
             </div>
           </nav>
