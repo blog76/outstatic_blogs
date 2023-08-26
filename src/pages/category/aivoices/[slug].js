d7d4ba7995ef85ffc101d6ef430bf64a78401345
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2023-08-26 17:58:13 +0530

    remove unneeded changes

--- src/pages/category/aivoices/[slug].js
+++ src/pages/category/aivoices/[slug].js
@@ -69,56 +69,6 @@ const BlogSingle = ({ post, htmlData }) => {
           </div>
         </div>
       </div>
-      <div className="sm:grid grid-cols-12 gap-7 rounded-xl w-full items-center my-12">
-        {[1, 2, 3].map((item, ind) => (
-          <div
-            key={ind}
-            className="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 bg-white border border-gray-200 rounded-lg shadow text-dark mt-5 sm:mt-0"
-          >
-            <a href="#">
-              <img
-                className="rounded-t-lg w-full h-[200px]"
-                src={
-                  "https://images.unsplash.com/photo-1682685797088-283404e24b9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                }
-                alt=""
-              />
-            </a>
-            <div class="p-5">
-              <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-black">
-                  Noteworthy technology acquisitions 2021
-                </h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
-              <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  class="w-3.5 h-3.5 ml-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
     </>
   );
 };
